6ebc0a49e8076290364e7745d512551ae31c7cad
rmsimao <rafaelmarinhosimao@gmail.com>
2026-01-08 20:42:02 -0300

    Questão 4

---
+++ AP-01/Questao03.py
@@ -0,0 +1,5 @@
+comprimento = 12
+altura = 14
+largura = 20
+volume = comprimento * altura * largura
+print(f"O volume do retangulo com Comprimento {comprimento}cm, Largura {largura}cm e Altura {altura}cm é {volume}cm³!")
