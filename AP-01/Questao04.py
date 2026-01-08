9d1a7ec193303077a3509f0cedf01748560e243e
rmsimao <rafaelmarinhosimao@gmail.com>
2026-01-08 20:45:27 -0300

    questao 4

---
+++ AP-01/Questao04.py
@@ -0,0 +1,7 @@
+nome_produto = "Cadeira Infantil"
+preco_produto = 12.40
+quantidade = 3
+
+valor_final = preco_produto * quantidade
+
+print(f"A compra de {quantidade} unidades de {nome_produto} é R${valor_final:.2f}.")
