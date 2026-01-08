73d642a6f8363271f5fadbf6c462acd014052d08
rmsimao <rafaelmarinhosimao@gmail.com>
2026-01-08 20:51:17 -0300

    q4

--- AP-01/Questao04.py
+++ AP-01/Questao04.py
@@ -4,4 +4,4 @@ quantidade = 3
 
 valor_final = preco_produto * quantidade
 
-print(f"A compra de {quantidade} unidades de {nome_produto} é R${valor_final:.2f}.")
+print(f"A compra de {quantidade} unidades de {nome_produto} com valor unitário {preco_produto}, é R${valor_final:.2f}.")
