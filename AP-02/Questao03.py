4e05065dbae5936f6174937b41b123614026da4c
rmsimao <rafaelmarinhosimao@gmail.com>
2026-01-24 13:36:33 -0300

    Exercicio 3 Atividade 2

---
+++ AP-02/Questao03.py
@@ -0,0 +1,10 @@
+nota1 = 7.5
+nota2 = 8.0
+nota3 = 6.5
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f"Nota 1: {nota1:.2f}")
+print(f"Nota 2: {nota2:.2f}")
+print(f"Nota 3: {nota3:.2f}")
+print(f"Média final: {media:.2f}")
