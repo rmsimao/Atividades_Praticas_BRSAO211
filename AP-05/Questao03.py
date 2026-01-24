ba297fecc4513260a3587d272f2dbf58f5a31eae
rmsimao <rafaelmarinhosimao@gmail.com>
2026-01-24 14:23:31 -0300

    Q3 AP5

--- AP-05/Questao03.py
+++ AP-05/Questao03.py
@@ -1,4 +1,4 @@
-def calcular_preco_com_desconto(preco_original: float, percentual_desconto: float) -> float:
+def calcula_desconto(preco_original: float, percentual_desconto: float) -> float:
     valor_desconto = preco_original * (percentual_desconto / 100)
     preco_final = preco_original - valor_desconto
     return preco_final
@@ -7,7 +7,7 @@ def calcular_preco_com_desconto(preco_original: float, percentual_desconto: floa
 preco = float(input("Digite o preço do produto: R$ "))
 desconto = float(input("Digite o percentual de desconto (%): "))
 
-preco_final = calcular_preco_com_desconto(preco, desconto)
+preco_final = calcula_desconto(preco, desconto)
 
 print(f"Preço original: R$ {preco:.2f}")
 print(f"Desconto aplicado: {desconto:.0f}%")
